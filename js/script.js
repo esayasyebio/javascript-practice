aa0b1b8a8047f6c4d4bd51b950635a8fe53fbdfa
esayasyebio <93210970+esayasyebio@users.noreply.github.com>
2022-02-28 15:28:25 -0700

    add file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//I am a student of javascript for bignerrs
+
+
+/*lorem ipsum dolor sit amet, consect
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maxime beatae nemo iste libero ea, quis expedita sunt quidem reiciendis, nihil odio iure quibusdam. Numquam officiis perferendis aspernatur animi pariatur.*/
+console.log("ESAYAS YEBIO")
+console.log("MY FAVORITE FOOD IS ENJERA WITH DORO WET")
+
+
